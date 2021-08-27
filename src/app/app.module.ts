94236b7c65f086f86145085bda54507e4283a3fb
MilunovicN99 <milunovic.nemanja@uns.ac.rs>
2021-08-27 20:13:44 +0200

    commit for angular updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SmerComponent } from './components/smer/smer.component';
+import { ProjekatComponent } from './components/projekat/projekat.component';
+import { StudentComponent } from './components/student/student.component';
+import { GrupaComponent } from './components/grupa/grupa.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmerComponent,
+    ProjekatComponent,
+    StudentComponent,
+    GrupaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
