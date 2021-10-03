2c8a4b697c84e3f2b1251c55cb3ddf6097a7323c
MilunovicN99 <milunovic.nemanja@uns.ac.rs>
2021-10-03 13:13:05 +0200

    Heroku linkovi dodati

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,4 +1,10 @@
-export const STUDENT_URL = 'http://localhost:8083/student';
+/* export const STUDENT_URL = 'http://localhost:8083/student';
 export const SMER_URL = 'http://localhost:8083/smer';
 export const PROJEKAT_URL = 'http://localhost:8083/projekat';
 export const GRUPA_URL = 'http://localhost:8083/grupa';
+*/
+
+export const STUDENT_URL = 'https://rva-backend-it33.herokuapp.com/student';
+export const SMER_URL = 'https://rva-backend-it33.herokuapp.com/smer';
+export const PROJEKAT_URL = 'https://rva-backend-it33.herokuapp.com/projekat';
+export const GRUPA_URL = 'https://rva-backend-it33.herokuapp.com/grupa';
